fdb72313ce045a725e64256cd1b69bb9bd15f8ef
Bryan Gallo <bryangallo95@hotmail.com>
2024-06-20 00:10:00 -0500

    Component Form espacio para Topic

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,9 @@
+import Form from "./components/Form";
+
 export default function App() {
     return (
-        <>
-            <form>
-                <h2 className="text-emerald-600 font-black text-4xl max-sm:text-3xl text-center capitalize my-10">
-                    Carga tu archivo de Topic de Conferencia
-                </h2>
-                <div className="mb-4">
-                    <label className="block text-center text-xs sm:text-sm md:text-lg overflow-hidden">
-                        <input
-                            type="file"
-                            accept=".txt"
-                            name="txt"
-                            className="text-md text-slate-500 bg-slate-100 rounded-md p-4
-                                file:mr-4 file:py-2 file:px-4
-                                file:rounded-full file:border-0
-                                file:text-md file:font-semibold
-                                file:bg-violet-50 file:text-sky-700
-                                hover:file:bg-violet-100 file:hover:cursor-pointer text-center"
-                        />
-                    </label>
-                </div>
-                <div className="flex justify-center text-center gap-3 mx-auto">
-                    <input
-                        type="submit"
-                        className="bg-emerald-700 p-4 py-3 text-white font-bold rounded uppercase mb-3 mt-3 text-sm hover:cursor-pointer hover:bg-emerald-900 transition-colors"
-                        value="Generar Topic"
-                    />
-                </div>
-            </form>
-        </>
+        <main>
+            <Form />
+        </main>
     );
 }
