8d8e06667100b80556c6bc73f28caaeada1bab38
Bryan Gallo <bryangallo95@hotmail.com>
2024-06-20 00:31:14 -0500

    Usando expressiones regulares para obtener los topics en un arreglo

--- src/hook/useForm.js
+++ src/hook/useForm.js
@@ -3,13 +3,20 @@ import { useState } from "react";
 const useForm = () => {
     const [file, setFile] = useState(null);
 
-    const readFile =  (file) => {
+    const readFile = (file) => {
         console.log(file);
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            const fileContent = e.target.result;
+            console.log(fileContent);
+            const matches = fileContent.match(/(.+?) (\d+min)/g);
+            console.log(matches);
+        };
+
+        reader.readAsText(file);
     };
 
-
-    return { file, setFile,readFile };
-    
+    return { file, setFile, readFile };
 };
 
 export default useForm;
